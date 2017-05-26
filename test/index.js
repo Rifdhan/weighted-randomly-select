992c859a40e50eed3bb5fc8da10cf13047edbcdb
Rifdhan Nazeer <rifdhan.nazeer@gmail.com>
2017-05-25 22:47:26 -0400

    Remove done() calls from unit tests
    Listing it as a function argument in it() blocks means you must
    call it - so just don't list it as an argument.

--- test/index.js
+++ test/index.js
@@ -7,28 +7,25 @@ const Randomly = require("../index");
 
 describe("Randomly", function() {
     describe("select()", () => {
-        it("should throw an error when called with no arguments", (done) => {
+        it("should throw an error when called with no arguments", () => {
             (() => {
                 Randomly.select();
             }).should.throw(Error, /please provide a non-empty array/);
-            done();
         });
 
-        it("should throw an error when argument is not an array", (done) => {
+        it("should throw an error when argument is not an array", () => {
             (() => {
                 Randomly.select({});
             }).should.throw(Error, /please provide a non-empty array/);
-            done();
         });
 
-        it("should throw an error when argument is an empty array", (done) => {
+        it("should throw an error when argument is an empty array", () => {
             (() => {
                 Randomly.select([]);
             }).should.throw(Error, /please provide a non-empty array/);
-            done();
         });
 
-        it("should throw an error when entries in the array do not have either the 'chance' or 'result' field", (done) => {
+        it("should throw an error when entries in the array do not have either the 'chance' or 'result' field", () => {
             (() => {
                 Randomly.select([{
                     result: {}
@@ -39,28 +36,25 @@ describe("Randomly", function() {
                     chance: 1
                 }]);
             }).should.throw(Error, /each array entry must be an object with 'chance' and 'result' properties/);
-            done();
         });
 
-        it("should throw an error when a 'chance' field is not a number", (done) => {
+        it("should throw an error when a 'chance' field is not a number", () => {
             (() => {
                 Randomly.select([{
                     chance: {}, result: {}
                 }]);
             }).should.throw(Error, /'chance' must be a positive number/);
-            done();
         });
 
-        it("should throw an error when a 'chance' field is a negative number", (done) => {
+        it("should throw an error when a 'chance' field is a negative number", () => {
             (() => {
                 Randomly.select([{
                     chance: -1, result: {}
                 }]);
             }).should.throw(Error, /'chance' must be a positive number/);
-            done();
         });
 
-        it("should throw an error when all chances add up to zero", (done) => {
+        it("should throw an error when all chances add up to zero", () => {
             (() => {
                 Randomly.select([{
                     chance: 0, result: {},
@@ -68,10 +62,9 @@ describe("Randomly", function() {
                     chance: 0, result: {},
                 }]);
             }).should.throw(Error, /all results have zero weight/);
-            done();
         });
 
-        it("should return an entry when given a valid array", (done) => {
+        it("should return an entry when given a valid array", () => {
             Sinon.stub(Math, "random").returns(0.4);
             Randomly.select([{
                     chance: 1, result: "a"
@@ -85,17 +78,15 @@ describe("Randomly", function() {
                     chance: 2, result: "b"
                 }
             ]).should.equal("b");
-            done();
         });
 
-        it("shouldn't throw an error when a result has zero chance", (done) => {
+        it("shouldn't throw an error when a result has zero chance", () => {
             Randomly.select([{
                     chance: 0, result: "a"
                 }, {
                     chance: 1, result: "b"
                 }
             ]).should.equal("b");
-            done();
         });
     });
 });
